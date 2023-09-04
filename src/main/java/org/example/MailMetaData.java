3cfef1dc73adef590c76060a75c2835eb52940fc
Sachin Shukla <104413867+Sach42@users.noreply.github.com>
2023-09-04 19:43:20 +0530

    Update MailMetaData.java

--- src/main/java/org/example/MailMetaData.java
+++ src/main/java/org/example/MailMetaData.java
@@ -7,7 +7,7 @@ public class MailMetaData {
     public static final String authPerms = "mail.smtp.auth";
 
     public static final String myUserMail = "SachinKumar05282@gmail.com";
-    public static final String password = "egbamczrgvcsoumn";//app password
+    public static final String password = "";//app password
     public static final String ReceiverMail = "Sachinshukla625@gmail.com";
 
 
